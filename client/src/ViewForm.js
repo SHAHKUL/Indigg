7703b243026b68889e661c8ce8e2714bcd25a415
SHAHKUL <shahkulhameed@gmail.com>
2023-05-29 22:19:28 +0530

    first commit

---
+++ client/src/ViewForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function ViewForm() {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">
+        +Create
+      </button>
+
+      <table>
+        <tr>
+   
+          <th>Team</th>
+          <th>Run Rate</th>
+          <th>Win</th>
+          <th>Lose</th>
+         
+        </tr>
+
+        <tr>
+         
+          <td>csk</td>
+          <td>0.125</td>
+          <td>5</td>
+          <td>5</td>
+          <td>
+            <Link>
+              <button type="button" className="btn btn-success">
+                Edit
+              </button>
+            </Link>
+            <button type="button" className="btn btn-danger">
+              Delete
+            </button>
+          </td>
+        </tr>
+
+        {/* ) */}
+
+        {/* })} */}
+      </table>
+    </div>
+  );
+}
+
+export default ViewForm;
